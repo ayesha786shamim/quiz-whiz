35547e03a40331df72054bb61acb630d5ab4ce52
Ayesha Shamim <ayeshashamim.011@gmail.com>
2025-07-10 17:05:32 +0500

    Add App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
+import Home from './Pages/Home';
+import Quiz from './Pages/Quiz';
+import ScorePage from './Pages/ScorePage';
+import ErrorPage from './Pages/ErrorPage';
 
-function App() {
+const App = () => {
+  const [amount, setAmount] = useState(10);
+  const [difficulty, setDifficulty] = useState('easy');
+  const [category, setCategory] = useState(9);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={
+          <Home
+            setAmount={setAmount}
+            setDifficulty={setDifficulty}
+            setCategory={setCategory}
+          />
+        } />
+        <Route path="/quiz" element={
+          <Quiz amount={amount} difficulty={difficulty} category={category} />
+        } />
+
+        <Route path="/score" element={<ScorePage />} />
+
+        <Route path="/error" element={<ErrorPage />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
